7e2bcc13103151eb224933a4ab8239aff6f106ec
Daniel Ciorea <cioreadani@yahoo.com>
2020-04-18 19:53:48 +0300

    Updates

---
+++ precache-manifest.183ef78628f9657c66a716ace77f83ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ae9d731983c14cbc683c72d06c26d9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "beb03c441b10360b888e",
+    "url": "/monsters-rolodex/static/css/main.fc42a1ef.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb03c441b10360b888e",
+    "url": "/monsters-rolodex/static/js/main.40aab4c9.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
